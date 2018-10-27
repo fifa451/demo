09ad55494f303ee80645dd2f30a11013a465a1a6
Raul <raul0301@gmail.com>
2018-10-27 11:26:21 +1000

    update some setting

--- juice-bar/ansible/aws-infra/create_prod.sh
+++ juice-bar/ansible/aws-infra/create_prod.sh
@@ -3,5 +3,7 @@
 ansible-playbook -vv -i inventory-prod aws.yml -l aws-vpc
 ansible-playbook -vv -i inventory-prod aws.yml -l aws-subnets
 ansible-playbook -vv -i inventory-prod aws.yml -l aws-igw
-ansible-playbook -vv -i inventory-prod aws.yml -l aws-route-table
+ansible-playbook -vv -i inventory-prod aws.yml -l aws-route-tables
 ansible-playbook -vv -i inventory-prod aws.yml -l aws-security-groups
+ansible-playbook -vv provision_ec2.yml
+ansible-playbook -i 54.252.186.83, install_app.yml
