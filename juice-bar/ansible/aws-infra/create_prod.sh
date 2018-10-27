3a1e6703a5ef03ab303c5b0619136c7946cdc1ad
Raul <raul0301@gmail.com>
2018-10-27 12:39:45 +1000

    Cleanup

--- juice-bar/ansible/aws-infra/create_prod.sh
+++ juice-bar/ansible/aws-infra/create_prod.sh
@@ -6,4 +6,4 @@ ansible-playbook -vv -i inventory-prod aws.yml -l aws-igw
 ansible-playbook -vv -i inventory-prod aws.yml -l aws-route-tables
 ansible-playbook -vv -i inventory-prod aws.yml -l aws-security-groups
 ansible-playbook -vv provision_ec2.yml
-ansible-playbook -i 54.252.186.83, install_app.yml
+ansible-playbook -i ip, install_app.yml
